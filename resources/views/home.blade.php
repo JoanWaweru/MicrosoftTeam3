2ce49e4adfe3a4e90f05cf7547cf80feb034c125
JoanWaweru <joan.waweru@strathmore.edu>
2021-12-06 17:35:47 +0300

    Update Patient Profile Working

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
 
         <!-- Sidebar -->
         @include('patient.sidebar')
-        
+
         <div class="col-xl-9 col-lg-8 col-md-12 col-sm-12 tm-content">
 
             <!-- section 1 -->
@@ -26,7 +26,16 @@
                     <p class="mb-5 tm-font-big">Welcome to our application.</p>
                     <a href="{{route('myprofile')}}" class="btn tm-btn tm-font-big">My Profile</a>
                     <a href="{{route('medicalHistory')}}" class="btn tm-btn tm-font-big">Medical History</a>
-                    <a href="{{route('logout')}}" class="btn tm-btn tm-font-big">Log Out</a>
+{{--                    <a href="{{route('logout')}}" class="btn tm-btn tm-font-big">Log Out</a>--}}
+                    <a class="btn tm-btn tm-font-big" href="{{ route('logout') }}"
+                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        {{ __('Log Out') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
                     <!-- data-nav-link holds the ID of nav item, which means this link should behave the same as that nav item  -->
                 </div>
             </section>
--- resources/views/patient/show.blade.php
+++ resources/views/patient/show.blade.php
@@ -6,7 +6,7 @@
             <div class="card p-3 py-4" style="background: #2f3037;">
                 <div class="text-center"> <img src="{{asset("assets/images/users/".Auth::user()->profile_photo)}}" width='100' class="rounded-circle">
                     <h3 class="mt-2">{{ $user->name }}</h3>City: <span class="mt-1 clearfix">{{ $user->city }}</span> Phone Number: <small class="mt-4">{{ $user->phone_number }}</small></br>
-                    <a href={{ route('myprofileUpdate',$user)}} class="btn btn-primary" type="button">Edit Profile</a>
+                    <a href={{ route('myprofileEdit',$user->id)}} class="btn btn-primary" type="button">Edit Profile</a>
                     <a href={{ route('home')}} class="btn btn-primary" type="button" >Back to Homepage</a>
 
                     {{--                    <div class="social-buttons mt-5"> <button class="neo-button"><i class="fa fa-facebook fa-1x"></i> </button> <button class="neo-button"><i class="fa fa-linkedin fa-1x"></i></button> <button class="neo-button"><i class="fa fa-google fa-1x"></i> </button> <button class="neo-button"><i class="fa fa-youtube fa-1x"></i> </button> <button class="neo-button"><i class="fa fa-twitter fa-1x"></i> </button> </div>--}}
