cfd25fb25bfb054800faa031d2a4f08e88e994d8
JoanWaweru <joan.waweru@strathmore.edu>
2021-12-12 20:42:15 +0300

    Logout button added to doctor and nurse

--- resources/views/layouts/doctor.blade.php
+++ resources/views/layouts/doctor.blade.php
@@ -97,7 +97,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="{{asset("storage/profilePhotos/".Auth::user()->profile_photo)}}" class="img-circle elevation-2" alt="User Image">
+          <img src="{{asset("storage/profilePhotos/".Auth::user()->profile_photo)}}" class="img-circle elevation-2">
         </div>
         <div class="info">
           <a href="#" class="d-block">{{Auth::user()->name}}</a>
